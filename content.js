6f12d8b7149162bc794f347bc3cbcb163f016285
ChillRock <jackjoe111111@gmail.com>
2021-04-24 03:31:42 -0500

    properly acquires image source

--- content.js
+++ content.js
@@ -1,1 +1,19 @@
-document.addEventListener('click', () => alert('Click occurred!'));
+document.addEventListener('click', function(e) {
+    e = e || window.event;
+    var target = e.target,
+        text = target.textContent || target.innerText;
+    function download(data, filename, type) {
+        var file = new Blob([data], {type: type});
+            var a = document.createElement("a"),
+                    url = URL.createObjectURL(file);
+            a.href = url;
+            a.download = filename;
+            document.body.appendChild(a);
+            a.click();
+            setTimeout(function() {
+                document.body.removeChild(a);
+                window.URL.revokeObjectURL(url);  
+            }, 0); 
+    }
+    alert(e.target.currentSrc);
+})
