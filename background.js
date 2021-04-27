359893fab5f1fdba1148f7c47fe1097243c06ff7
jzkensik <jzkensik@gmail.com>
2021-04-27 02:25:22 -0500

    added twitter functionality/cleaned up code

--- background.js
+++ background.js
@@ -1,21 +1,7 @@
 
 chrome.runtime.onMessage.addListener(
     function(arg, sender, sendResponse) {
-    //   var args=arg.collection;
-    //   for (i in args){
-    //   var img_url=args[i];
-    //   try{
-    //    saveas=img_url.replace(/[^a-zA-Z0-9]/g,'-');
-    //   }
-    //   catch (problem){
-    //   }
-    function onStartedDownload(id) {
-        console.log(`Started downloading: ${id}`);
-      }
-      
-      function onFailed(error) {
-        console.log(`Download failed: ${error}`);
-      }
+
     var browser = window.chrome || window.browser;
     var downloading = browser.downloads.download({
         url: arg
@@ -30,7 +16,7 @@ chrome.runtime.onMessage.addListener(
     // //    filename: saveas,
     //   saveAs: false
     //   });
-    //Promise.resolve("").then(result => sendResponse(arg));
+    Promise.resolve("").then(result => sendResponse('image grabbed successfully'));
     return true;
      });
   
