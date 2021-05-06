fbd716ad093800f9589b7c3ddff288078e208f73
jzkensik <jzkensik@gmail.com>
2021-05-05 22:35:36 -0500

    changed icon effect in preparation of location_change update. finally fixed laptop

--- content.js
+++ content.js
@@ -1,37 +1,3 @@
-// document.addEventListener('click', function(e) {
-//     e = e || window.event;
-//     var target = e.target;
-//     document.addEventListener('keydown', function(f) {
-//         console.log(e.target.currentSrc)
-//         alert(e.target.currentSrc + '    ' + f.key);
-//     })
-//     function download(data, filename, type) {
-//         var file = new Blob([data], {type: type});
-//             var a = document.createElement("a"),
-//                     url = URL.createObjectURL(file);
-//             a.href = url;
-//             a.download = filename;
-//             document.body.appendChild(a);
-//             a.click();
-//             setTimeout(function() {
-//                 document.body.removeChild(a);
-//                 window.URL.revokeObjectURL(url);  
-//             }, 0); 
-//     }
-// })
-
-// document.addEventListener('click', function() {
-//     console.log(document.getElementsByTagName('img')[0])
-// })
-// for(item of document.getElementsByTagName('img')) {
-//     item.addEventListener('keypress', function(f) {
-//         item.addEventListener('mouseover', function(e) {
-//             e = e || window.event;
-//             // var target = e.target;
-//             console.log(f.target + '    ' + e.target + '  gottem');
-//         })
-//     })
-// }
 var img_url = '';
 document.addEventListener('mouseover', simulateClick);
 function simulateClick(e) {
@@ -56,20 +22,10 @@ function simulateClick(e) {
       }
 
 }
-    // document.addEventListener('mouseover', function(e) {
-    //     // if(f.key == '`') {
-    //     //     console.log(e.target.currentSrc)
-    //     //     console.log(f.target + '  ' + f.key + '  gotten')
-    //     // }
-    // })
+function changeLocation(e){
+console.log('hey check this out')
+}
 
-// document.addEventListener('click', function() {
-//     console.log(typeof document.getElementsByTagName('img'))
-//     console.log(document.getElementsByTagName('img'))
-//     // document.getElementsByTagName('img').forEach(item => {
-//     //     console.log(item)
-//     // })
-// })
 // document.getElementsByTagName('img').forEach(item => {
 //     item.addEventListener('keypress', function(f) {
 //         item.addEventListener('mouseover', function(e) {
